be765c3b1b96dc39b3669b101472f11784a7d1f8
JoeAyoub22 <joe.n.ayoub18@gmail.com>
2023-08-20 12:56:20 +0300

    Finished function display stats + some modifications to my code

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
+class Employee:
+  def __init__(self,employee_id,username,start_date,gender,salary):
+    self.employee_id = employee_id
+    self.username = username
+    self.start_date = start_date
+    self.gender = gender
+    self.salary = salary
+
+
+
+def import_employees(filename):
+#references:
+# https://www.pythontutorial.net/python-basics/python-read-text-file/)
+# https://stackoverflow.com/questions/51977258/appending-data-from-a-text-file-into-a-list-in-python
+    employees = []
+    with open('employees.txt', 'r') as file:
+        for line in file:
+            employee_id, username, start_date, gender, salary = line.strip().split(', ')
+            employees.append(Employee(employee_id, username, start_date, gender,int(salary)))
+    return employees
+
+def display_stats(employees):
+  male_count = 0
+  female_count = 0
+  for employee in employees:
+    gender = employee.gender
+    if gender == "male":
+      male_count +=1
+    else:
+      female_count +=1
+  print("The number of male employees is: " , male_count)
+  print("The number of female employees is: " , female_count)
+
 print("Welcome user")
+employees = import_employees("employees.txt")
 login_attempts = 0
 admin_correct_username = "admin"
 admin_correct_password = "admin123123"
@@ -30,30 +64,10 @@ while Userinputed_username == "admin" and Userinputed_passowrd == "admin123123":
       print("6. Raise Employee's Salary")
       print("7. Exit")
       choice=int(input())
+
+      if choice == 1:
+        display_stats(employees)
   menu()
-
-
-
-class Employee:
-  def __init__(self,employee_id,username,start_date,gender,salary):
-    self.emp_id = employee_id
-    self.username = username
-    self.start_date = start_date
-    self.gender = gender
-    self.salary = salary
-
-def importemployees():
-#references:
-# https://www.pythontutorial.net/python-basics/python-read-text-file/)
-# https://stackoverflow.com/questions/51977258/appending-data-from-a-text-file-into-a-list-in-python
-    employees = []
-    with open('employees.txt', 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            employee_id, username, start_date, gender, salary = line.strip().split(', ')
-            employees.append(Employee(employee_id, username, start_date, gender,int(salary)))
-    return employees
-
 
 
 
